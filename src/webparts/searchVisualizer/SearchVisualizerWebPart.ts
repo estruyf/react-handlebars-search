4f6e5bb17fee7060c8282652b13cfddd754b1087
Elio Struyf <info@estruyf.be>
2018-09-25 10:05:00 -0400

    Demo start

--- src/webparts/searchVisualizer/SearchVisualizerWebPart.ts
+++ src/webparts/searchVisualizer/SearchVisualizerWebPart.ts
@@ -23,15 +23,14 @@ import { UserProfileService } from './services/UserProfileService';
 export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISearchVisualizerWebPartProps> {
 
   /**
-   * Renders the search visualizer component
-   */
+  * Renders the search visualizer component
+  */
   public render(): void {
     // Load the core UI Fabric styles
     SPComponentLoader.loadCss('https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/9.6.0/css/fabric-9.6.0.scoped.min.css');
 
     const element: React.ReactElement<ISearchVisualizerProps> = React.createElement(
-      SearchVisualizer,
-      {
+      SearchVisualizer, {
         ...this.properties,
         sorting: this.getSortingOption(),
         audienceTargetingBooleanOperator: this.properties.audienceTargetingBooleanOperator ? this.properties.audienceTargetingBooleanOperator : 'OR',
@@ -58,8 +57,8 @@ export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISear
   }
 
   /**
-   * Webpart Property Pane Configuration
-   */
+  * Webpart Property Pane Configuration
+  */
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
@@ -71,91 +70,6 @@ export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISear
             {
               groupName: strings.QueryGroupName,
               groupFields: [
-                PropertyFieldCollectionData("advancedSearch", {
-                  key: "advancedSearch",
-                  label: "Advanced search",
-                  panelHeader: "Advanced search",
-                  manageBtnLabel: "Define advanced search filter",
-                  value: this.properties.advancedSearch,
-                  fields: [
-                    {
-                      id: "name",
-                      title: "Name",
-                      type: CustomCollectionFieldType.string,
-                      required: true
-                    },
-                    {
-                      id: "filter",
-                      title: "Filter",
-                      type: CustomCollectionFieldType.dropdown,
-                      options:[
-                        {
-                          text: "Contains",
-                          key: SearchFilter.contains
-                        },
-                        {
-                          text: "Contains starts with",
-                          key: SearchFilter.containsStartsWith
-                        },
-                        {
-                          text: "Equals",
-                          key: SearchFilter.equals
-                        },
-                        {
-                          text: "Starts with",
-                          key: SearchFilter.startsWith
-                        },
-                        {
-                          text: "Less than",
-                          key: SearchFilter.lessThan
-                        },
-                        {
-                          text: "Greater than",
-                          key: SearchFilter.greaterThan
-                        },
-                        {
-                          text: "Not contains",
-                          key: SearchFilter.notContains
-                        },
-                        {
-                          text: "Not equals",
-                          key: SearchFilter.notEquals
-                        },
-                        {
-                          text: "Not starts with",
-                          key: SearchFilter.notStartsWith
-                        }
-                      ],
-                      defaultValue: SearchFilter.contains
-                    },
-                    {
-                      id: "value",
-                      title: "Value",
-                      type: CustomCollectionFieldType.string,
-                      required: true
-                    },
-                    {
-                      id: "operator",
-                      title: "Operator",
-                      type: CustomCollectionFieldType.dropdown,
-                      options:[
-                        {
-                          text: "",
-                          key: null
-                        },
-                        {
-                          text: "AND",
-                          key: "AND"
-                        },
-                        {
-                          text: "OR",
-                          key: "OR"
-                        }
-                      ]
-                    }
-                  ],
-                  disabled: false
-                }),
                 PropertyPaneTextField('query', {
                   label: strings.QueryFieldLabel,
                   description: strings.QueryFieldDescription,
@@ -169,40 +83,8 @@ export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISear
                   min: 1,
                   max: 50
                 }),
-                PropertyFieldCollectionData("mpSorting", {
-                  key: "mpSorting",
-                  label: "Defined managed property sorting",
-                  panelHeader: "Defined managed property sorting",
-                  manageBtnLabel: "Manage sorting",
-                  value: this.properties.mpSorting,
-                  fields: [
-                    {
-                      id: "mpName",
-                      title: "Name",
-                      type: CustomCollectionFieldType.string,
-                      required: true,
-                      onGetErrorMessage: this.validateSortingProperty,
-                      deferredValidationTime: 500
-                    },
-                    {
-                      id: "mpOrder",
-                      title: "Sort order",
-                      type: CustomCollectionFieldType.dropdown,
-                      options: [
-                        {
-                          key: "ascending",
-                          text: "Ascending"
-                        },
-                        {
-                          key: "descending",
-                          text: "Descending"
-                        }
-                      ],
-                      defaultValue: "ascending",
-                      required: true
-                    }
-                  ],
-                  disabled: false
+                PropertyPaneTextField('sorting', {
+                  label: strings.SortingFieldLabel
                 }),
                 PropertyPaneToggle('duplicates', {
                   label: strings.DuplicatesFieldLabel,
@@ -215,7 +97,7 @@ export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISear
                   offText: strings.PrivateGroupsFieldLabelOff
                 })
               ],
-              isCollapsed: true
+              isCollapsed: false
             },
             {
               groupName: strings.TemplateGroupName,
@@ -314,12 +196,12 @@ export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISear
   }
 
   /**
-   * Property pane field changes
-   *
-   * @param propertyPath
-   * @param oldValue
-   * @param newValue
-   */
+  * Property pane field changes
+  *
+  * @param propertyPath
+  * @param oldValue
+  * @param newValue
+  */
   protected onPropertyPaneFieldChanged(propertyPath: string, oldValue: any, newValue: any) {
     if (propertyPath === "advancedSearch") {
       // Remove the old value
@@ -337,28 +219,28 @@ export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISear
   }
 
   /**
-   * Create the keyword filter query for the provided row
-   *
-   * @param item
-   */
+  * Create the keyword filter query for the provided row
+  *
+  * @param item
+  */
   private createFilterQuery(item: IAdvancedFilter) {
     switch (item.filter) {
       case SearchFilter.containsStartsWith:
-        return `${item.name}:${item.value}* ${item.operator || ""}`.trim();
+      return `${item.name}:${item.value}* ${item.operator || ""}`.trim();
       case SearchFilter.startsWith:
-        return `${item.name}=${item.value}* ${item.operator || ""}`.trim();
+      return `${item.name}=${item.value}* ${item.operator || ""}`.trim();
       case SearchFilter.notContains:
-        return `-${item.name}:${item.value} ${item.operator || ""}`.trim();
+      return `-${item.name}:${item.value} ${item.operator || ""}`.trim();
       case SearchFilter.notStartsWith:
-        return `-${item.name}=${item.value}* ${item.operator || ""}`.trim();
+      return `-${item.name}=${item.value}* ${item.operator || ""}`.trim();
       default:
-        return `${item.name}${item.filter}${item.value} ${item.operator || ""}`.trim();
+      return `${item.name}${item.filter}${item.value} ${item.operator || ""}`.trim();
     }
   }
 
   /**
-   * Returns the sorting options in the right format
-   */
+  * Returns the sorting options in the right format
+  */
   private getSortingOption() {
     const { mpSorting } = this.properties;
     if (mpSorting && mpSorting.length > 0) {
@@ -368,12 +250,12 @@ export default class SearchVisualizerWebPart extends BaseClientSideWebPart<ISear
   }
 
   /**
-   * Check if the provided managed property is sortable
-   *
-   * @param value
-   * @param index
-   * @param crntItem
-   */
+  * Check if the provided managed property is sortable
+  *
+  * @param value
+  * @param index
+  * @param crntItem
+  */
   private validateSortingProperty = async (value: any, index: number, crntItem: any): Promise<string> => {
     if (value) {
       try {
